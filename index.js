9a7b05aaa44e170cb049db486bc17724291f6049
rajxode <rajsh1202@gmail.com>
2023-08-25 00:58:54 +0530

    crete folder sturcture and fire up express server

---
+++ index.js
@@ -0,0 +1,34 @@
+
+// exporting all the function and libraries of express
+const express=require('express');
+
+// path variable
+const path=require('path');
+
+// port id
+const port=1000;
+
+
+// app having all the function of express and firing up our framework 
+const app=express();
+// settting EJS
+app.set('view engine','ejs');
+
+// defining path of result file with respect to current file
+app.set('views',path.join(__dirname,'views'));
+
+
+// returning responses  { get(path ,function ) }
+app.get('/',function(request, response){
+    return response.render('home');
+});
+
+
+// starting server
+app.listen(port,function(err){
+    if(err){
+        console.log('Error',err);
+        return;
+    }
+    console.log("Hello World");
+});
