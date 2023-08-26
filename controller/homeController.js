24b5b93620556c57fcda36193e0d97ef11e0a6e0
rajxode <rajsh1202@gmail.com>
2023-08-27 01:07:36 +0530

    update the controllers, as the DB schema is updated

--- controller/homeController.js
+++ controller/homeController.js
@@ -10,11 +10,18 @@ module.exports.home= function(req,res){
 
 // create a new Habit inside the database
 module.exports.createHabit =async function(request,response){
+
+    let date = new Date().toString();
+    date =`${date.slice(4,15)}`;
+    const weekStatus = Array(7).fill(null);
     
     // creating new element in mongodb
     const doc = Habits.create({
     // getting the value of name and phone
         name:request.body.name,
+        createdAt:date,
+        weeklyStatus:weekStatus,
+        completedDays:0
     });
     return response.redirect('back');
 };
--- controller/myHabitsController.js
+++ controller/myHabitsController.js
@@ -1,16 +1,36 @@
 
 const Habits = require('../models/habits');
 
+// to get list of all the days and dates in last week
+const CalculateDayOfWeek = (date) => {
+    // array storing all the dates and day
+    var days = new Array();
+    // storing values in asceding order of date
+    for (var i = 6; i >= 0; i--){
+        // store values in the form of string
+        days[6-i] = new Date(date.getFullYear(), date.getMonth(), date.getDate() - i).toString();
+        days[6-i] = `${days[6-i].slice(0,4)}, ${days[6-i].slice(4,15)}`;
+    }
+    // return the array of dates
+    return days;
+}
+
 
 module.exports.home =async function(req,res){
     let date = new Date().toString();
     date =`${date.slice(0,3)},${date.slice(3,15)}`;
-
+    const pastWeek = CalculateDayOfWeek(new Date());
     // rendering all the contacts stored inside the database
     const myHabits = await Habits.find({});
 
     return res.render('myHabits',{
         date:date,
-        myHabits:myHabits
+        myHabits:myHabits,
+        weekDays:pastWeek
     });
+}
+
+
+module.exports.toggleStatus = async function(req,res){
+    let id = req.query.id;
 }
